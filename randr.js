06fb6ec313baf6db08e4b85c10c0f81eac44db71
DenyZhirkov <denyzhirkov@yandex.ru>
2015-05-27 14:59:36 +0300

    Update randr.js

--- randr.js
+++ randr.js
@@ -8,7 +8,7 @@
 		k.implement = (k.implement==undefined || k.implement==true)? true : false;
 		k.extend = (k.extend==undefined || k.extend==true)? true : false;
 		var node = (k.node==undefined)? this.nodes('div') : this.nodes(k.node);
-		if(k.defaults!==undefined && k.defaults!==false){
+		if(k.defaults!==undefined){
 			for (var i = k.defaults.length - 1; i >= 0; i--){
 				node.setAttribute(k.defaults[i].type, k.defaults[i].data || '');
 			};
@@ -19,7 +19,11 @@
 				k.content.reverse();
 				for(var i = k.content.length - 1; i >= 0; i--){
 					if(k.defaults!==undefined && k.implement!==false && k.content[i].extend!==false){
-						k.content[i].defaults + k.defaults;
+						if(k.content[i].defaults!==undefined){
+							k.content[i].defaults + k.defaults;
+						}else{
+							k.content[i].defaults = k.defaults;
+						}
 					}
 					node.appendChild(render(k.content[i]));
 				}
@@ -27,9 +31,13 @@
 			//object
 			else if(k.content instanceof Object){
 				if(k.defaults!==undefined && k.implement!==false && k.content.extend!==false){
-					k.content.defaults + k.defaults;
+					if(k.content.defaults!==undefined){
+						k.content.defaults + k.defaults;
+					}else{
+						k.content.defaults = k.defaults;
+					}
 				}
-				node.innerHTML = render(k.content);
+				node.appendChild(render(k.content));
 			}
 			//string
 			else{
