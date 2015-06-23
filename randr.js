ee2be424e28248cd9e3aa44635fb7a4715826c01
DenyZhirkov <denyzhirkov@yandex.ru>
2015-06-23 13:19:18 +0300

    Update randr.js
    1.0.1

--- randr.js
+++ randr.js
@@ -1,23 +1,28 @@
 /* randr: a js library, which makes html dom from json */
 /* author: denyzhirkov@yandex.ru @dolphin4ik */
-/* version: 0.4.1 */
+/* version: 1.0.1 */
 ;var randr = function(json,nodes,type){
-	this.version = '0.4.1';
+	this.version = '1.0.1';
 	this.nodes = function(t){return document.createElement(t);}
 	var flag = true;
 	var stack = function(a,b){
-		var c = '';
-		var stack = [];
-		for (var i in b) {
-			if(b[i].type!='class'){stack.push({type:b[i].type,data:b[i].data});}
-			else{c = b[i].data;}
-		};
-		for (var i in a) {
-			if(a[i].type!='class'){stack.push({type:a[i].type,data:a[i].data});}
-			else{c += ' '+a[i].data;}
-		};
-		stack.push({type:'class',data:c});
-		return stack;
+		if(a instanceof Array){
+			var c = '';
+			var stack = [];
+			for (var i in b) {
+				if(b[i].type!='class'){stack.push({type:b[i].type,data:b[i].data});}
+				else{c = b[i].data;}
+			};
+			for (var i in a) {
+				if(a[i].type!='class'){stack.push({type:a[i].type,data:a[i].data});}
+				else{c += ' '+a[i].data;}
+			};
+			stack.push({type:'class',data:c});
+			return stack;
+		}else{
+			for(var i in b){ a[i] = (i=='class' && a[i])? b[i]+' '+a[i] : b[i]; };
+			return a;
+		}
 	}
 	var render = function(k){
 		k.implement = (k.implement==undefined || k.implement==false)? false : true;
@@ -29,8 +34,12 @@
 			if(nodes!=false && nodes!=undefined && nodes[k.node]!=undefined){node=nodes[k.node](k.content);flag=false;}else{node=this.nodes(k.node);}
 		}
 		if(k.defaults!==undefined){
-			for (var i = k.defaults.length - 1; i >= 0; i--){
-				node.setAttribute(k.defaults[i].type, k.defaults[i].data || '');
+			for (var i in k.defaults){
+				if(k.defaults instanceof Array){
+					node.setAttribute(k.defaults[i].type, k.defaults[i].data || '')
+				}else{
+					node.setAttribute(i, k.defaults[i] || '');
+				}
 			};
 		}
 		//events !demo
